82b165d13862af017125144c11ee9f2fdcbe682f
Zeno <s102215@ap.be>
2019-09-19 09:46:56 +0200

    Try 2 cha,ge ignorefile

---
+++ OefLes1/VS2017/TestVS/TestVS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Console.WriteLine("It seems to work >:D");
+            Console.ReadLine();
+        }
+    }
+}
