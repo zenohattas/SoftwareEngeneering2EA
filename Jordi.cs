57e2a224e1c5776c3d54031575729ade2011030c
GrinningJack <ekcuoc.idroj@gmail.com>
2019-09-19 09:56:43 +0200

    Add files via upload

---
+++ Jordi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Labo1ConsoleAppGithub
+{
+    class JordiCoucke
+    {
+        private int Jordle;
+
+        public int jordle
+        {
+            get { return Jordle; }
+            set { Jordle = value; }
+        }
+
+    }
+}
