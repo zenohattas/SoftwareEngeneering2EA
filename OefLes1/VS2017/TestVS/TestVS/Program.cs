4f871b1ec5d6c25ec65fb84d7bf2fbba42b9098f
JordiCoucke <s104623@ap.be>
2019-09-19 09:50:11 +0200

    Test

--- OefLes1/VS2017/TestVS/TestVS/Program.cs
+++ OefLes1/VS2017/TestVS/TestVS/Program.cs
@@ -13,6 +13,7 @@ namespace TestVS
             Console.WriteLine("Test");
             Console.WriteLine("It seems to work >:D");
             Console.WriteLine("Like those damn Mexicans");
+            Console.WriteLine("test");
             Console.ReadLine();
         }
     }
