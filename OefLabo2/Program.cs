5ffd14f0e2e4d034d2611fd0d2f8944ecbd624bf
Zeno <s102215@ap.be>
2019-09-27 09:06:50 +0200

    Starting new project, oefening labo 2

---
+++ OefLabo2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OefLabo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
