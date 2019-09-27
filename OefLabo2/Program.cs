cdd515854d0878a30a69e361fbb3d92d54094565
JordiCoucke <s104623@ap.be>
2019-09-27 09:09:33 +0200

    muda muda muda!

--- OefLabo2/Program.cs
+++ OefLabo2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace OefLabo2
 {
@@ -6,7 +6,7 @@ namespace OefLabo2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Woraldo!");
         }
     }
 }
