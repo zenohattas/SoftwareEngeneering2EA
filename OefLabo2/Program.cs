1cdb16f684c6f388f77fc997e01e6261776229de
koen Christianen <koen.christianen@student.ap.be>
2019-09-27 09:12:35 +0200

    o/

--- OefLabo2/Program.cs
+++ OefLabo2/Program.cs
@@ -6,7 +6,7 @@ namespace OefLabo2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("hi o/");
         }
     }
 }
