3107dab8d5b8c8e67d07146e714a6179b1441332
Bart <s110266@ap.be>
2019-09-27 09:13:09 +0200

    Driehoek print ding

--- OefLabo2/Program.cs
+++ OefLabo2/Program.cs
@@ -6,7 +6,13 @@ namespace OefLabo2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 0;i<10;i++){
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
