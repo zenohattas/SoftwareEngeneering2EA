04200293235c54bd2f6699d7543345b5847b3026
Shania Peeters <shania.peeters@student.ap.be>
2019-09-27 09:20:17 +0200

    Carl method toegevoegd

--- OefLabo2/Program.cs
+++ OefLabo2/Program.cs
@@ -6,8 +6,14 @@ namespace OefLabo2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(">:D");
+         
+        }
+
+        void Carl()
+        {
+            Console.WriteLine("Paul: Carl, there is a dead human in our house.");
+            Console.WriteLine("Carl: Oh,hey,how did it get there?");
+            Console.WriteLine("Paul:Caaarrrrllll,what did you do ;^;");
         }
     }
 }
