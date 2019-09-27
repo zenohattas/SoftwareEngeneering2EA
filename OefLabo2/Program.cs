9efcebe3eefbc77473d9d19b587763b05812c5bb
Bart <s110266@ap.be>
2019-09-27 09:28:27 +0200

    Abstracte klasse draw, tekent symbool op locatie

--- OefLabo2/Program.cs
+++ OefLabo2/Program.cs
@@ -6,13 +6,7 @@ namespace OefLabo2
     {
         static void Main(string[] args)
         {
-            for(int i = 0;i<10;i++){
-                for(int j = 0; j < i; j++)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-            }
+            Draw.draw(30, 20, '*');
         }
     }
 }
